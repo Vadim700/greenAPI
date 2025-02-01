183a5528c8f2d732f355bde922b1d47988dd7da0
Вадим Бекмансуров <vadimbekmansurov@Vadims-MacBook-Pro.local>
2025-02-01 16:01:22 +0500

    added singing message

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,15 +1,25 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
 import { MessageForm } from '../ui/MessageForm';
+import { sendMessage } from '@/servises/messages';
 
 interface Props {
   className?: string;
 }
 
 export const Footer: React.FC<Props> = ({ className }) => {
-  const sendMessage = async (values: unknown) => {
+  const instanse = process.env.ID_INSTANCE || '';
+  const token = process.env.API_TOKEN_INSTANCE || '';
+
+  const sendingMessage = async (values: { message: string }) => {
     'use server';
-    console.log(values);
+    await sendMessage(instanse, token, '89085728793', values.message);
+
+    console.log(
+      await sendMessage(instanse, token, '89085728793', values.message),
+      ' => ',
+      values.message,
+    );
   };
   return (
     <footer
@@ -18,7 +28,10 @@ export const Footer: React.FC<Props> = ({ className }) => {
         'flex items-center py-2.5 px-4 min-h-[64px] bg-accentbg text-3xl',
       )}
     >
-      <MessageForm placeholder="Введите сообщение" sendMessage={sendMessage} />
+      <MessageForm
+        placeholder="Введите сообщение"
+        sendMessage={sendingMessage}
+      />
     </footer>
   );
 };
---
+++ servises/messages.ts
@@ -0,0 +1,83 @@
+// import axios from 'axios';
+
+const BASE_URL = 'https://api.green-api.com';
+
+export const sendMessage = async (
+  idInstance: string,
+  apiTokenInstance: string,
+  phoneNumber: string,
+  message: string,
+) => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/waInstance${idInstance}/SendMessage/${apiTokenInstance}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          chatId: `${phoneNumber}@c.us`,
+          message,
+        }),
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error('Не получилось отправить сообщение');
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error sending message:', error);
+    throw error;
+  }
+};
+
+export const receiveNotification = async (
+  idInstance: string,
+  apiTokenInstance: string,
+) => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/waInstance${idInstance}/ReceiveNotification/${apiTokenInstance}`,
+      {
+        method: 'GET',
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error('Не получиловь получить уведомление');
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error('Error receiving notification:', error);
+    throw error;
+  }
+};
+
+export const deleteNotification = async (
+  idInstance: string,
+  apiTokenInstance: string,
+  receiptId: number,
+) => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/waInstance${idInstance}/DeleteNotification/${apiTokenInstance}/${receiptId}`,
+      {
+        method: 'DELETE',
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error('Error deleting notification:', error);
+    throw error;
+  }
+};
